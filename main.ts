fa7610d9668ebd3c914f63678723067cefd3c6e4
Aoki-Yudai <52954197+Aoki-Yudai@users.noreply.github.com>
2022-11-30 09:22:19 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    Tello.fly_or_land()
+})
+basic.forever(function () {
+    Tello.Setorder(0)
+})
