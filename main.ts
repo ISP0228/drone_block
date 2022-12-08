8173127c9a4dfdabcc1018cd3fb3f35f1a0f2954
Aoki-Yudai <52954197+Aoki-Yudai@users.noreply.github.com>
2022-12-08 12:44:51 +0900

    更新 pxt.json, main.blocks, main.ts, custom.ts, main.py

--- main.ts
+++ main.ts
@@ -1,27 +1,16 @@
-input.onButtonPressed(Button.A, function () {
-    Tello.gomid(
-    100,
-    100,
-    100,
-    50,
-    1
-    )
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    Tello.gomid(100, 100, 100, 50, 1)
 })
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     Tello.fly_or_land()
 })
-input.onButtonPressed(Button.B, function () {
-    Tello.curve(
-    30,
-    0,
-    30,
-    60,
-    0,
-    60,
-    50
-    )
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    Tello.curve(30, 0, 30, 60, 0, 60, 50)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
+    radio.sendString("ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい")
 })
 Tello.radiosetgroup(31)
-basic.forever(function () {
+basic.forever(function on_forever() {
     Tello.Setorder(1000)
 })
